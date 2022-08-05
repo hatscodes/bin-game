d93b9969d035e5a17e12542ddcbac83975e7d71e
hatscodes <110644039+hatscodes@users.noreply.github.com>
2022-08-04 20:56:14 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,12 @@
 from tkinter import * 
 game = Tk()
 
-canvas = Canvas(game, width = 100, height = 100)
-canvas.create_text(text = "works")
+game.geometry('500x400')
+game.title("bin game")
+
+
+
+#canvas = Canvas(game, width = 100, height = 100)
+#canvas.create_text(Text = "works")
 
 game.mainloop()
